bc59091324f731c4f335857ec7537db881e4d14f
Andrey Side <sideandrey@gmail.com>
2020-06-05 15:51:02 +0300

    new

---
+++ BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public static int calculate(int price) {
+        int miles = price / 20;
+        return miles;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Miles modified
+
+Testing repository for MilesModified task in frame of javaqa-05 netology course
