fd15056bf0a09453f32f424acbd5639c43d693a7
Andrey Side <sideandrey@gmail.com>
2021-02-06 23:43:42 +0300

    new

---  Main.java
+++  Main.java
@@ -1,10 +1,8 @@
 public class Main {
-    public static void main(String[] args)
-    {
-            BonusMilesService service = new BonusMilesService();
+    public void main(String[] args) { 
+        BonusMilesService service = new BonusMilesService();
             int price = 12150;
             int miles = service.calculate(price);
-            System.out.println(miles);
-        }
-
+            System.out.println(miles); 
+    }
 }
--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,6 +1,6 @@
-public class BonusMilesService {
-    public static int calculate(int price) {
+public class BonusMilesService { 
+    public int calculate(int price) { 
         int miles = price / 20;
-        return miles;
+        return miles; 
     }
 }
